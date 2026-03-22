868f30dcecda43da9bae5fc87819d702e47bbebd
AB0219 <bollepalliainesh19@gmail.com>
2026-03-22 23:15:13 +0530

    Update queries.sql

--- part1-rdbms/queries.sql
+++ part1-rdbms/queries.sql
@@ -1,1 +1,9 @@
-
+## Answers
+### Q1
+SELECT c.customer_name, SUM(p.unit_price * oi.quantity) AS total_value
+FROM Customers c
+JOIN Orders o ON c.customer_id = o.customer_id
+JOIN Order_Items oi ON o.order_id = oi.order_id
+JOIN Products p ON oi.product_id = p.product_id
+WHERE c.customer_city = 'Mumbai'
+GROUP BY c.customer_name;
