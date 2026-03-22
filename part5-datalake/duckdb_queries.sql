4b5f0b5f8245fea71f34b6f4033443dfec3640c9
AB0219 <bollepalliainesh19@gmail.com>
2026-03-23 00:24:59 +0530

    Update duckdb_queries.sql

--- part5-datalake/duckdb_queries.sql
+++ part5-datalake/duckdb_queries.sql
@@ -1,5 +1,5 @@
 --Answers
--- Q1
+-- Q1— Customers + total number of orders
 SELECT 
     c.customer_id,
     c.customer_name,
@@ -9,7 +9,7 @@ LEFT JOIN read_json_auto('datasets/orders.json') o
 ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.customer_name;
 
--- Q2
+-- Q2— Top 3 customers by total order value
 SELECT 
     c.customer_id,
     c.customer_name,
@@ -21,18 +21,18 @@ GROUP BY c.customer_id, c.customer_name
 ORDER BY total_spent DESC
 LIMIT 3;
 
--- Q3
+-- Q3— Products purchased by Bangalore customers
 SELECT DISTINCT p.product_name
 FROM read_csv_auto('datasets/customers.csv') c
 JOIN read_json_auto('datasets/orders.json') o
 ON c.customer_id = o.customer_id
 JOIN read_parquet('datasets/products.parquet') p
-ON TRUE   -- assuming no direct key, adjust if product_id exists
+ON TRUE   
 WHERE c.city = 'Bangalore';
 
 ON o.product_id = p.product_id
 
--- Q4
+-- Q4— Join all three files
 SELECT 
     c.customer_name,
     o.order_date,
@@ -42,4 +42,4 @@ FROM read_csv_auto('datasets/customers.csv') c
 JOIN read_json_auto('datasets/orders.json') o
 ON c.customer_id = o.customer_id
 JOIN read_parquet('datasets/products.parquet') p
-ON TRUE;  -- adjust if product_id exists
+ON TRUE;  
